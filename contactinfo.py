de852cccda6ea4368119f94a4e17f57c2d735a9b
Tom Bourke <Tom.bourke@ericsson.com>
2024-08-20 11:52:05 +0100

    database changes

--- contactinfo.py
+++ contactinfo.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-# Database configuration
+# Database configuration test
 USERNAME = 'root'
 PASSWORD = ''
 HOST = 'localhost'
